cebfb98784add0197c46a6f5beea7f6b1e22c650
Kevin Hodge <kevinhodge@Kevins-Air.attlocal.net>
2022-03-06 21:29:26 -0600

    Initial commit

---
+++ sync_files_functions.py
@@ -0,0 +1,17 @@
+"""
+Sync Files Project: Functions
+-----------------------------
+Contains the functions for the Sync Files Project.
+"""
+
+
+import os
+
+
+def get_working_directory():
+    """
+    Gets the current working directory.
+    :return: string
+        current working directory
+    """
+    return os.getcwd()
