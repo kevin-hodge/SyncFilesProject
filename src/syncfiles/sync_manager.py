9bde46905e0eb0c5b0a0db09ea0194d2b574ccbb
Kevin Hodge <khod18@gmail.com>
2022-06-26 20:30:18 -0500

    Added prelim tests for sync_manager.

--- src/syncfiles/sync_manager.py
+++ src/syncfiles/sync_manager.py
@@ -3,5 +3,23 @@
 Author: Kevin Hodge
 """
 
+from typing import List
+from syncfiles.file_structure import FileStructure
+
+
 class SyncManager:
-    pass
+    """Takes FileStructures as inputs and synchronizes them.
+
+    Condition: If FILE or FOLDER exists in one FileStructure, but not the other, and it IS marked as UPDATED.
+    Action: It should be COPIED from the FileStructure that it exists in to the other FileStructure.
+
+    Condition: If FILE or FOLDER exists in one FileStructure, but not the other, and it IS NOT marked as UPDATED.
+    Action: It should be DELETED from the FileStructure that it exists in.
+
+    Condition: If FILE or FOLDER exists in both FileStructures and IS marked as UPDATED in ONLY ONE FileStructure.
+    Action: It should be DELETED from the FileStructure in which it IS NOT marked as UPDATED and COPIED from the
+        FileStructure in which it IS marked as UPDATED to the other FileStructure.
+
+    """
+    def __init__(self, fstruct_list: List[FileStructure]) -> None:
+        pass
--- tests/test_sync_manager.py
+++ tests/test_sync_manager.py
@@ -4,11 +4,36 @@ Author: Kevin Hodge
 """
 
 import unittest
+from typing import List
+import tests.tfuncs as tfuncs
+from tests.tfuncs import TFunctions
 from syncfiles.sync_manager import SyncManager
+from syncfiles.file_structure import FileStructure
 
 
 class SyncManagerTestCase(unittest.TestCase):
-    pass
+    """Test case for ConfigManager"""
+    def __init__(self, *args, **kwargs) -> None:
+        self.tf: TFunctions = TFunctions()
+        super().__init__(*args, **kwargs)
+
+    @tfuncs.handle_test_dirs
+    def test_no_updates(self):
+        test_dir1: str = str(self.tf.test_path1)
+        self.tf.create_rand_fstruct(test_dir1)
+        test_fstruct1: FileStructure = FileStructure(test_dir1)
+
+        test_dir2: str = str(self.tf.test_path2)
+        test_fstruct2: FileStructure = FileStructure(test_dir2)
+
+        fstruct_list: List[FileStructure] = [test_fstruct1, test_fstruct2]
+        SyncManager(fstruct_list)
+
+    @tfuncs.handle_test_dirs
+    def test_new_file(self):
+        # test_dir1: str = str(self.tf.test_path1)
+        # test_dir2: str = str(self.tf.test_path2)
+        pass
 
 
 if __name__ == "__main__":
